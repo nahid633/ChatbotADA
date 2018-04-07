54eb83d5c5407313776b089e7af47e379302b400
nahid.guliyev <nahid.guliyev@knexel.com>
2018-04-07 18:17:58 +0400

    chat+landingpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {ChatMessageComponent} from './chat-message/chat-message.component';
+import {ChatNavBarComponent} from './chat-nav-bar/chat-nav-bar.component';
+import {ChatWindowComponent} from './chat-window/chat-window.component';
+import {ChatPageComponent} from './chat-page/chat-page.component';
+import {FromNowPipe} from './pipes/from-now.pipe';
+import {UsersService} from './user/users.service';
+import {ThreadsService} from './thread/threads.service';
+import {MessagesService} from './message/messages.service';
+import {TabletMenuComponent} from './tablet-menu/tablet-menu.component';
+import {MatButtonModule, MatIconModule} from '@angular/material';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatMessageComponent,
+    ChatNavBarComponent,
+    ChatWindowComponent,
+    ChatPageComponent,
+    FromNowPipe,
+    TabletMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [    MessagesService, ThreadsService, UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
